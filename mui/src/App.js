afbf0198964c12abcdfe954a15092018e3a95faf
paudel54 <2sansrit@gmail.com>
2023-06-14 20:08:48 +0545

    configured MUI with react along with react router dom

---
+++ mui/src/App.js
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Testing the MUI button </p>
+      <Button variant="contained">
+        Click here!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
